d67f8c64e939f584b361463ffcc54cd996001166
Lucio Solórzano <lucio.solorzano@gmail.com>
2020-08-07 08:43:34 -0500

    login pre alfa

--- TransCarga/login.cs
+++ TransCarga/login.cs
@@ -1,10 +1,9 @@
 ﻿using System;
-using System.Data;
 using System.Windows.Forms;
 using MySql.Data.MySqlClient;
 using System.Configuration;
-using System.Text;
 using System.Drawing;
+using System.Runtime.InteropServices;
 
 namespace TransCarga
 {
@@ -24,7 +23,13 @@ namespace TransCarga
         {
             InitializeComponent();
         }
-
+        /*
+        // arrastrar el form por la pantalla
+        [DllImport("user32.DLL", EntryPoint = "ReleaseCapture")]
+        private extern static void ReleaseCapture();
+        [DllImport("user32.DLL", EntryPoint = "SendMessage")]
+        private extern static void SendMessage(System.IntPtr hwnd, int wmsg, int wparam, int lparam);
+        */
         private void login_Load(object sender, EventArgs e)
         {
             this.Text = this.Text + "- Versión " + System.Diagnostics.FileVersionInfo.GetVersionInfo(Application.ExecutablePath).FileVersion;
@@ -32,94 +37,99 @@ namespace TransCarga
             lb_titulo.BackColor = System.Drawing.Color.White;
             //lb_titulo.Parent = pictureBox1;
             Image logo = Image.FromFile("recursos/logo_solorsoft_2p.png");
-            Image salir = Image.FromFile("recursos/exit48.png");
-            Image entrar = Image.FromFile("recursos/ok.png");
+            Image salir = Image.FromFile("recursos/Close_32.png");
+            //Image entrar = Image.FromFile("recursos/ok.png");
             pictureBox1.Image = logo;
             Button2.Image = salir;
-            Button1.Image = entrar;
+            Button2.ImageAlign = ContentAlignment.MiddleCenter;
+            //Button1.Image = entrar;
             init();
+            // jala datos de configuracion
+            jaladatos();
+            //
             Tx_user.Focus();
         }
-
         private void init()
         {
             checkBox1.Visible = false;
             tx_newcon.Visible = false;
             tx_newcon.MaxLength = 10;
+            //
+            this.BackColor = System.Drawing.ColorTranslator.FromHtml(Program.colbac);
         }
-
         private void Button1_Click(object sender, EventArgs e)
         {
             // validamos los campos
-            string usuari = this.Tx_user.Text;     // usuario
-            //string contra = this.Tx_pwd.Text;      // passw
-            string contra = lib.md5(Tx_pwd.Text);
-            if (usuari == "")
+            string usuari = Tx_user.Text.Trim();     // usuario
+            if (usuari == "" || usuari == "USUARIO")
             {
-                MessageBox.Show("Por favor, ingrese el usuario", "Atención");
+                MessageBox.Show("Por favor, ingrese el nombre de usuario", "Atención");
+                Tx_user.Focus();
                 return;
             }
-            if (contra == "")
+            if (Tx_pwd.Text.Trim() == "" || Tx_pwd.Text == "CLAVE")
             {
                 MessageBox.Show("Por favor, ingrese la contraseña", "Atención");
+                Tx_pwd.Focus();
                 return;
             }
-            try
+            if (Tx_user.Text != "USUARIO" && Tx_pwd.Text != "CLAVE")
             {
-                MySqlConnection cn = new MySqlConnection(DB_CONN_STR);
-                cn.Open();
-                //validamos que el usuario y passw son los correctos
-                string query = "select a.bloqueado,a.local,a.nombre " +
-                    "from usuarios a " +
-                    "where a.nom_user=@usuario and a.pwd_user=@contra";
-                MySqlCommand mycomand = new MySqlCommand(query, cn);
-                mycomand.Parameters.AddWithValue("@usuario", Tx_user.Text);
-                mycomand.Parameters.AddWithValue("@contra", contra);
-                MySqlDataReader dr = mycomand.ExecuteReader();
-                if (dr.HasRows)
+                try
                 {
-                    if (dr.Read())
+                    string contra = lib.md5(Tx_pwd.Text);
+                    MySqlConnection cn = new MySqlConnection(DB_CONN_STR);
+                    cn.Open();
+                    //validamos que el usuario y passw son los correctos
+                    string query = "select a.bloqueado,a.local,a.nombre " +
+                        "from usuarios a " +
+                        "where a.nom_user=@usuario and a.pwd_user=@contra";
+                    MySqlCommand mycomand = new MySqlCommand(query, cn);
+                    mycomand.Parameters.AddWithValue("@usuario", Tx_user.Text);
+                    mycomand.Parameters.AddWithValue("@contra", contra);
+                    MySqlDataReader dr = mycomand.ExecuteReader();
+                    if (dr.HasRows)
                     {
-                        if (dr.GetString(0) == "0")
+                        if (dr.Read())
                         {
-                            TransCarga.Program.vg_user = Tx_user.Text;
-                            TransCarga.Program.vg_nuse = dr.GetString(2);
-                            TransCarga.Program.almuser = dr.GetString(1);
-                            dr.Close();
-                            // cambiamos la contraseña si fue hecha
-                            cambiacont();
-                            // jala datos de configuracion
-                            jaladatos();
-                            // nos vamos al form principal
-                            Program.vg_user = this.Tx_user.Text;
-                            main Main = new main();
-                            Main.Show();
-                            this.Hide();
-                        }
-                        else
-                        {
-                            dr.Close();
-                            MessageBox.Show("El usuario esta Bloqueado!");
-                            return;
+                            if (dr.GetString(0) == "0")
+                            {
+                                TransCarga.Program.vg_user = Tx_user.Text;
+                                TransCarga.Program.vg_nuse = dr.GetString(2);
+                                TransCarga.Program.almuser = dr.GetString(1);
+                                dr.Close();
+                                // cambiamos la contraseña si fue hecha
+                                cambiacont();
+                                // nos vamos al form principal
+                                Program.vg_user = this.Tx_user.Text;
+                                main Main = new main();
+                                Main.Show();
+                                this.Hide();
+                            }
+                            else
+                            {
+                                dr.Close();
+                                MessageBox.Show("El usuario esta Bloqueado!");
+                                return;
+                            }
                         }
                     }
+                    else
+                    {
+                        dr.Close();
+                        MessageBox.Show("Usuario y/o Contraseña erronea", " Atención ", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        return;
+                    }
+                    cn.Close();
                 }
-                else
+                catch (MySqlException ex)
                 {
-                    dr.Close();
-                    MessageBox.Show("Usuario y/o Contraseña erronea", " Atención ", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    MessageBox.Show(ex.Message, "No se tiene conexión con el servidor", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    Application.Exit();
                     return;
                 }
-                cn.Close();
-            }
-            catch (MySqlException ex)
-            {
-                MessageBox.Show(ex.Message, "No se tiene conexión con el servidor", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                Application.Exit();
-                return;
             }
         }
-
         private void Button2_Click(object sender, EventArgs e)
         {
             const string mensaje = "Deseas salir del sistema?";
@@ -137,12 +147,64 @@ namespace TransCarga
                 Tx_pwd.Focus();
             }
         }
+        private void Tx_user_Enter(object sender, EventArgs e)
+        {
+            if (Tx_user.Text == "USUARIO")
+            {
+                Tx_user.Text = "";
+                Tx_user.ForeColor = Color.Black;
+            }
+        }
+        private void Tx_user_Leave(object sender, EventArgs e)
+        {
+            if (Tx_user.Text.Trim() == "")
+            {
+                Tx_user.Text = "USUARIO";
+                Tx_user.ForeColor = Color.Gray;
+            }
+        }
 
         private void Tx_pwd_KeyPress(object sender, KeyPressEventArgs e)
         {
             if (e.KeyChar == Convert.ToChar(Keys.Enter))
             {
                 Button1.PerformClick();
+            }
+        }
+        private void Tx_pwd_TextChanged(object sender, EventArgs e)
+        {
+            if (panel1.Visible == true)
+            {
+                if (Tx_pwd.Text.Trim() != "" && Tx_pwd.Text.Trim() != "CLAVE")
+                {
+                    checkBox1.Visible = true;
+                    checkBox1.Checked = false;
+                    tx_newcon.Visible = false;
+                }
+                else
+                {
+                    checkBox1.Visible = false;
+                    checkBox1.Checked = false;
+                    tx_newcon.Visible = false;
+                }
+            }
+        }
+        private void Tx_pwd_Enter(object sender, EventArgs e)
+        {
+            if (Tx_pwd.Text == "CLAVE")
+            {
+                Tx_pwd.Text = "";
+                Tx_pwd.ForeColor = Color.Black;
+                Tx_pwd.UseSystemPasswordChar = true;
+            }
+        }
+        private void Tx_pwd_Leave(object sender, EventArgs e)
+        {
+            if (Tx_pwd.Text.Trim() == "")
+            {
+                Tx_pwd.Text = "CLAVE";
+                Tx_pwd.ForeColor = Color.Gray;
+                Tx_pwd.UseSystemPasswordChar = false;
             }
         }
 
@@ -159,24 +221,6 @@ namespace TransCarga
                 tx_newcon.Visible = false;
             }
         }
-
-        private void Tx_pwd_TextChanged(object sender, EventArgs e)
-        {
-            if (this.panel1.Visible == true)
-            {
-                if (Tx_pwd.Text != "")
-                {
-                    checkBox1.Visible = true;
-                    checkBox1.Checked = false;
-                }
-                else
-                {
-                    checkBox1.Visible = false;
-                    checkBox1.Checked = false;
-                }
-            }
-        }
-
         private void cambiacont()
         {
             if (checkBox1.Checked == true && tx_newcon.Text != "")
@@ -232,8 +276,8 @@ namespace TransCarga
                         // usuario puede cambiar su contraseña?
                         if (dr.GetString(0) == "chpwd")
                         {
-                            if (dr.GetString(1) == "1") this.panel1.Visible = true;
-                            else this.panel1.Visible = false;
+                            if (dr.GetString(1) == "1") panel1.Visible = true;
+                            else panel1.Visible = false;
                         }
                         // obtenemos la configuración de los colores
                         if (dr.GetString(0).StartsWith("color") == true)
@@ -256,6 +300,16 @@ namespace TransCarga
                 return;
             }
             cn.Close();
+        }
+        private void login_KeyDown(object sender, KeyEventArgs e)
+        {
+            //ReleaseCapture();
+            //SendMessage(this.Handle, 0x112, 0xf012, 0);
+        }
+        private void checkBox1_CheckStateChanged(object sender, EventArgs e)
+        {
+            if(checkBox1.Checked == true) tx_newcon.Visible = true;
+            else tx_newcon.Visible = false;
         }
     }
 }
--- TransCarga/login.designer.cs
+++ TransCarga/login.designer.cs
@@ -33,32 +33,31 @@
             this.label2 = new System.Windows.Forms.Label();
             this.Tx_pwd = new System.Windows.Forms.TextBox();
             this.Tx_user = new System.Windows.Forms.TextBox();
-            this.Label3 = new System.Windows.Forms.Label();
-            this.Label1 = new System.Windows.Forms.Label();
-            this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.tx_newcon = new System.Windows.Forms.TextBox();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.lb_titulo = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.Button2 = new System.Windows.Forms.Button();
             this.Button1 = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
             this.groupBox1.SuspendLayout();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox1
             // 
+            this.groupBox1.BackColor = System.Drawing.Color.Transparent;
             this.groupBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
             this.groupBox1.Controls.Add(this.label2);
             this.groupBox1.Controls.Add(this.Tx_pwd);
             this.groupBox1.Controls.Add(this.Tx_user);
-            this.groupBox1.Controls.Add(this.Label3);
-            this.groupBox1.Controls.Add(this.Label1);
             this.groupBox1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.groupBox1.Location = new System.Drawing.Point(88, 207);
+            this.groupBox1.Location = new System.Drawing.Point(271, 166);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(284, 79);
+            this.groupBox1.Size = new System.Drawing.Size(284, 75);
             this.groupBox1.TabIndex = 27;
             this.groupBox1.TabStop = false;
             // 
@@ -74,50 +73,26 @@
             // 
             // Tx_pwd
             // 
-            this.Tx_pwd.Location = new System.Drawing.Point(117, 47);
+            this.Tx_pwd.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.Tx_pwd.Location = new System.Drawing.Point(66, 46);
             this.Tx_pwd.Name = "Tx_pwd";
-            this.Tx_pwd.Size = new System.Drawing.Size(134, 20);
-            this.Tx_pwd.TabIndex = 17;
-            this.Tx_pwd.UseSystemPasswordChar = true;
+            this.Tx_pwd.Size = new System.Drawing.Size(166, 20);
+            this.Tx_pwd.TabIndex = 1;
+            this.Tx_pwd.Text = "CLAVE";
             this.Tx_pwd.TextChanged += new System.EventHandler(this.Tx_pwd_TextChanged);
+            this.Tx_pwd.Enter += new System.EventHandler(this.Tx_pwd_Enter);
+            this.Tx_pwd.Leave += new System.EventHandler(this.Tx_pwd_Leave);
             // 
             // Tx_user
             // 
-            this.Tx_user.Location = new System.Drawing.Point(117, 18);
+            this.Tx_user.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.Tx_user.Location = new System.Drawing.Point(66, 17);
             this.Tx_user.Name = "Tx_user";
-            this.Tx_user.Size = new System.Drawing.Size(134, 20);
-            this.Tx_user.TabIndex = 16;
-            // 
-            // Label3
-            // 
-            this.Label3.AutoSize = true;
-            this.Label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Label3.Location = new System.Drawing.Point(37, 50);
-            this.Label3.Name = "Label3";
-            this.Label3.Size = new System.Drawing.Size(77, 16);
-            this.Label3.TabIndex = 15;
-            this.Label3.Text = "Contraseña";
-            // 
-            // Label1
-            // 
-            this.Label1.AutoSize = true;
-            this.Label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Label1.Location = new System.Drawing.Point(37, 21);
-            this.Label1.Name = "Label1";
-            this.Label1.Size = new System.Drawing.Size(55, 16);
-            this.Label1.TabIndex = 14;
-            this.Label1.Text = "Usuario";
-            // 
-            // checkBox1
-            // 
-            this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(28, 5);
-            this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(117, 17);
-            this.checkBox1.TabIndex = 28;
-            this.checkBox1.Text = "Cambia contraseña";
-            this.checkBox1.UseVisualStyleBackColor = true;
-            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            this.Tx_user.Size = new System.Drawing.Size(166, 20);
+            this.Tx_user.TabIndex = 0;
+            this.Tx_user.Text = "USUARIO";
+            this.Tx_user.Enter += new System.EventHandler(this.Tx_user_Enter);
+            this.Tx_user.Leave += new System.EventHandler(this.Tx_user_Leave);
             // 
             // tx_newcon
             // 
@@ -129,60 +104,89 @@
             // 
             // panel1
             // 
-            this.panel1.Controls.Add(this.tx_newcon);
             this.panel1.Controls.Add(this.checkBox1);
-            this.panel1.Location = new System.Drawing.Point(88, 294);
+            this.panel1.Controls.Add(this.tx_newcon);
+            this.panel1.Location = new System.Drawing.Point(271, 246);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(284, 26);
             this.panel1.TabIndex = 29;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.checkBox1.Location = new System.Drawing.Point(26, 5);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(117, 17);
+            this.checkBox1.TabIndex = 29;
+            this.checkBox1.Text = "Cambia contraseña";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckStateChanged += new System.EventHandler(this.checkBox1_CheckStateChanged);
             // 
             // lb_titulo
             // 
             this.lb_titulo.BackColor = System.Drawing.Color.White;
             this.lb_titulo.Font = new System.Drawing.Font("Palatino Linotype", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lb_titulo.ForeColor = System.Drawing.Color.DimGray;
-            this.lb_titulo.Location = new System.Drawing.Point(260, 2);
+            this.lb_titulo.Location = new System.Drawing.Point(255, 36);
             this.lb_titulo.Name = "lb_titulo";
-            this.lb_titulo.Size = new System.Drawing.Size(186, 188);
+            this.lb_titulo.Size = new System.Drawing.Size(320, 132);
             this.lb_titulo.TabIndex = 0;
             this.lb_titulo.Text = "titulo";
             this.lb_titulo.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // pictureBox1
             // 
-            this.pictureBox1.BackColor = System.Drawing.Color.White;
-            this.pictureBox1.Location = new System.Drawing.Point(1, 0);
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Location = new System.Drawing.Point(35, 38);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(262, 190);
+            this.pictureBox1.Size = new System.Drawing.Size(200, 190);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 30;
             this.pictureBox1.TabStop = false;
             // 
             // Button2
             // 
-            this.Button2.Location = new System.Drawing.Point(105, 354);
+            this.Button2.BackColor = System.Drawing.Color.Transparent;
+            this.Button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Button2.ForeColor = System.Drawing.Color.Transparent;
+            this.Button2.Location = new System.Drawing.Point(537, -1);
             this.Button2.Name = "Button2";
-            this.Button2.Size = new System.Drawing.Size(53, 52);
+            this.Button2.Size = new System.Drawing.Size(37, 37);
             this.Button2.TabIndex = 26;
-            this.Button2.UseVisualStyleBackColor = true;
+            this.Button2.UseVisualStyleBackColor = false;
             this.Button2.Click += new System.EventHandler(this.Button2_Click);
             // 
             // Button1
             // 
-            this.Button1.Location = new System.Drawing.Point(304, 354);
+            this.Button1.BackColor = System.Drawing.Color.Gray;
+            this.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Button1.ForeColor = System.Drawing.Color.Transparent;
+            this.Button1.Location = new System.Drawing.Point(319, 284);
             this.Button1.Name = "Button1";
-            this.Button1.Size = new System.Drawing.Size(53, 52);
-            this.Button1.TabIndex = 25;
-            this.Button1.UseVisualStyleBackColor = true;
+            this.Button1.Size = new System.Drawing.Size(196, 34);
+            this.Button1.TabIndex = 0;
+            this.Button1.Text = "INGRESAR";
+            this.Button1.UseVisualStyleBackColor = false;
             this.Button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(22)))), ((int)(((byte)(204)))));
+            this.panel2.Controls.Add(this.pictureBox1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(254, 347);
+            this.panel2.TabIndex = 31;
             // 
             // login
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.Silver;
-            this.ClientSize = new System.Drawing.Size(448, 441);
-            this.Controls.Add(this.pictureBox1);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(15)))), ((int)(((byte)(15)))));
+            this.ClientSize = new System.Drawing.Size(574, 347);
+            this.Controls.Add(this.panel2);
             this.Controls.Add(this.lb_titulo);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.groupBox1);
@@ -193,14 +197,17 @@
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "login";
+            this.Opacity = 0.9D;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "SolorSoft TransCarga";
             this.Load += new System.EventHandler(this.login_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.login_KeyDown);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel2.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -210,16 +217,15 @@
         private System.Windows.Forms.GroupBox groupBox1;
         internal System.Windows.Forms.TextBox Tx_pwd;
         internal System.Windows.Forms.TextBox Tx_user;
-        internal System.Windows.Forms.Label Label3;
-        internal System.Windows.Forms.Label Label1;
         internal System.Windows.Forms.Button Button2;
         internal System.Windows.Forms.Button Button1;
-        private System.Windows.Forms.CheckBox checkBox1;
         internal System.Windows.Forms.TextBox tx_newcon;
         private System.Windows.Forms.Panel panel1;
         internal System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label lb_titulo;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.CheckBox checkBox1;
     }
 }
 
